610832f28544623019d630a9e8a03be2338dd09e
Julian Gruber <julian@juliangruber.com>
2013-08-02 21:44:27 -0400

    more tests for get

--- test/get.js
+++ test/get.js
@@ -1,13 +1,18 @@
-var test = require('tape');
+var tape = require('tape');
 var Cache = require('..');
 var MemDB = require('memdb');
 
-test('watch key', function (t) {
-  t.plan(4);
+function test (name, fn) {
+  tape.test(name, function (t) {
+    var source = MemDB();
+    var cache = MemDB();
+    var db = Cache(source, cache);
+    fn(t, db, source, cache);
+  });
+}
 
-  var source = MemDB();
-  var cache = MemDB();
-  var db = Cache(source, cache);
+test('key is watched', function (t, db, source, cache) {
+  t.plan(4);
 
   db.get('foo', function (err) {
     t.ok(err);
@@ -18,5 +23,52 @@ test('watch key', function (t) {
         t.equal(value, 'bar');
       });
     });
+  });
+});
+
+test('key is watched right number of times', function (t, db, source, cache) {
+  t.plan(5);
+
+  t.equal(db.ranges.length, 0);
+  db.get('foo', function (err) {
+    t.ok(err);
+    t.equal(db.ranges.length, 1);
+    db.get('foo', function (err) {
+      t.ok(err);
+      t.equal(db.ranges.length, 1);
+    });
+  });
+});
+
+test('double request result in only one operation', function (t, db, source, cache) {
+  t.plan(6);
+
+  source.put('foo', 'bar', function (err) {
+    t.error(err);
+
+    db.on('foo', function (value) {
+      t.equal(value, 'bar', 'event emitted');
+    });
+    db.get('foo', function (err, value) {
+      t.error(err);
+      t.equal(value, 'bar', 'got value');
+    });
+    db.get('foo', function (err, value) {
+      t.error(err);
+      t.equal(value, 'bar', 'got value');
+    });
+  });
+});
+
+test('cache hit cleans up getting array', function (t, db, source, cache) {
+  t.plan(4);
+
+  cache.put('foo', 'bar', function (err) {
+    db.get('foo', function (err, value) {
+      t.error(err);
+      t.equal(value, 'bar');
+      t.equal(db.getting.length, 0);
+    });
+    t.equal(db.getting.length, 1);
   });
 });
