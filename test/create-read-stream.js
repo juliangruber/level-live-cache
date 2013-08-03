7989072f63d568d95add7943e930a3b1bd19d983
Julian Gruber <julian@juliangruber.com>
2013-08-03 14:48:22 -0400

    small test refactor

--- test/create-read-stream.js
+++ test/create-read-stream.js
@@ -31,21 +31,17 @@ test('reads data from source', function (t, db, source, cache) {
 });
 
 test('caches data', function (t, db, source, cache) {
-  t.plan(4);
+  t.plan(2);
 
   source.put('foo', 'bar', function (err) {
     t.error(err);
     // make sure client has data
     db.createReadStream().on('end', function () {
       cache.createReadStream().pipe(through(write, end));
-
-      function write (kv) {
-        t.equal(kv.key, 'foo');
-        t.equal(kv.value, 'bar');
-      }
-
+      var res = [];
+      function write (kv) { res.push(kv) }
       function end () {
-        t.ok(true, 'ended');
+        t.deepEqual(res, [{ key: 'foo', value: 'bar' }]);
       }
     });
   });
